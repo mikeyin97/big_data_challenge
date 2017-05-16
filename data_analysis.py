f6d06213c51c83703c4022a165175fed81173cb3
mikeyin97 <yinjitong@hotmail.com>
2017-05-15 20:54:05 -0400

    add csv parsing

--- data_analysis.py
+++ data_analysis.py
@@ -1,11 +1,22 @@
 
 #Importing Relevant Libraries
 import matplotlib.pyplot as plt
-import csv
+import pandas as pd
 import random
 import datetime
 import numpy as np
 import math
-import os
 import cv2
-os.path.abspath("C:\Users\Michael\Documents\GitHub\big_data_challenge\datasets")
+import os
+import inspect
+import os
+
+#Setting Path
+module_path = inspect.getfile(inspect.currentframe())
+module_dir = os.path.realpath(os.path.dirname(module_path))
+#print(module_dir)
+
+#Making Dataframe
+df = pd.read_csv(module_dir+'/datasets/TB_burden_countries_2017-05-16.csv')
+
+
