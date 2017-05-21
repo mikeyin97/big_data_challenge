7ef354bfd378b0d06a7ef98cfe3f01fe723ed4f8
joshkang247 <joshk.kang@mail.utoronto.ca>
2017-05-21 12:42:07 -0400

    Created a line graph

--- sources/test.py
+++ sources/test.py
@@ -60,22 +60,15 @@ bar_width = 0.10
 index = np.arange(len(years))
 count = 0
 for region in regions:
+    """
     plt.bar(index+count*bar_width, results_norm[region], bar_width, alpha = 0.5, color=colors[region], label=region)
     count += 1
+    """
+    plt.plot(years, results_norm[region], alpha=0.5, color= colors[region], label=region)
 
 plt.xlabel('Year')
 plt.ylabel('Incidences per 100 000 population')
 plt.title('World Tuberculosis Incidences for the Past 15 Years')
-plt.xticks(index+bar_width*(len(regions)-1)/2, years)
+#plt.xticks(index+bar_width*(len(regions)-1)/2, years)
 plt.legend()
 plt.show()
-"""
-objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
-y_pos = np.arange(len(objects))
-performance = [10,8,6,4,2,1]
-plt.barh(y_pos, performance, align='center', alpha=0.5)
-plt.yticks(y_pos, objects)
-plt.xlabel('Usage')
-plt.title('Programming language usage')
-plt.show()
-"""
