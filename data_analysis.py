750b03e37fd55c59a1758974984b7dfbc15b01f2
Michael Yin <yinjitong@hotmail.com>
2017-05-23 16:40:23 -0400

    slight changes - added seaborn, removed warnings, etc

--- data_analysis.py
+++ data_analysis.py
@@ -6,18 +6,58 @@ import inspect
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from sklearn import datasets, linear_model
+import seaborn as sns
+import warnings
+
+# Initial calls
 plt.ion()
+sns.set()
+warnings.filterwarnings("ignore",".*GUI is implemented.*")
 
 # Function Definitions
 def log(column):        #takes the natural log of each value of the input column
+    column += 0.0001
     return (np.log2(column))
+
+def animate_one(col, size):
+    plt.legend(handles=[blue_patch, green_patch, red_patch, cyan_patch, mag_patch, yel_patch])
+    result_by_year = result[result['year'] == year]
+    plt.title(str(year) + " " + col)
+    plt.scatter(x = log(result_by_year[col]), y = log(result_by_year['e_inc_100k']), c = result["color"].values.tolist(), alpha = 0.8)
+    plt.axis(size)
+    plt.xlabel("log " + col)
+    plt.ylabel("log incidence/100k")
+    plt.draw()
+
+def animate_plot(col, axes):
+    while True:
+        for year in years:
+            plt.legend(handles=[blue_patch, green_patch, red_patch, cyan_patch, mag_patch, yel_patch])
+            result_by_year = result[result['year'] == year]
+            plt.title(year)
+            #plt.plot(log(result_by_year[col]),log(result_by_year['e_inc_100k']), "x", color=colors[(year-2000)%len(colors)])
+            plt.scatter(x = log(result_by_year[col]), y = log(result_by_year['e_inc_100k']), c = result["color"].values.tolist(), alpha = 0.8)
+            plt.axis(axes)
+            plt.xlabel("log pop dens")
+            plt.ylabel("log incidence/100k")
+            plt.draw()
+            plt.pause(pause_time)
+            plt.clf() #I have yet to figure out how to exit this loop
     
+
 # Constant Variables
 colors = ["b", "g", "r", "c", "m", "y", "k", "#22ff00"]         # Colour List to cycle
 axis_pop_dens = (-2, 20, -2, 12)                         # Axes Parameters
 axis_health_exp = (-6, 25, -5, 25)
 axis_gdp = (0, 20, -2, 20)
 pause_time = 0.5                                                # Time between graph cycles
+blue_patch = mpatches.Patch(color='b', label='Europe')
+green_patch = mpatches.Patch(color='g', label='Africa')
+red_patch = mpatches.Patch(color='r', label='Americas')
+cyan_patch = mpatches.Patch(color='c', label='Western Pacific')
+mag_patch = mpatches.Patch(color='m', label='Eastern Mediterranean')
+yel_patch = mpatches.Patch(color='y', label='Southeast Asia')
+
 
 # Setting Directory Path
 module_path = inspect.getfile(inspect.currentframe())
@@ -39,13 +79,13 @@ pop_den_data_less = pop_den_data[["Country Name", "Country Code", "2000", "2001"
 
 # Match health and gdp data to shape of TB Data DF
 health_data_less = pd.melt(health_data, var_name= "year", id_vars = ["Countries", "Indicators", "Units"], value_name = "Data")
-health_data_less = health_data_less.sort(['Countries', 'year'], ascending=True)
+health_data_less = health_data_less.sort_values(by = ['Countries', 'year'], ascending=True)
 health_data_less['year'] = health_data_less['year'].astype(int)
 health_data_less["Data"] = [float("NaN") if type(i) == str else i for i in health_data_less["Data"]]
 health_data_less = health_data_less.rename(columns={"Data": "Health Expenditure per capita"})
 
 gdp_data_less = pd.melt(gdp_data, var_name= "year", id_vars = ["Countries", "Indicators", "Units"], value_name = "Data")
-gdp_data_less = gdp_data_less.sort(['Countries', 'year'], ascending=True)
+gdp_data_less = gdp_data_less.sort_values(by = ['Countries', 'year'], ascending=True)
 gdp_data_less['year'] = gdp_data_less['year'].astype(int)
 gdp_data_less["Data"] = [float("NaN") if type(i) == str else i for i in gdp_data_less["Data"]]
 gdp_data_less = gdp_data_less.rename(columns={"Data": "GDP per capita"})
@@ -54,7 +94,7 @@ gdp_data_less = gdp_data_less.rename(columns={"Data": "GDP per capita"})
 # into a Variable
 pop_den_data_less = pd.melt(pop_den_data_less, id_vars=["Country Name", "Country Code"], 
                   var_name="Year", value_name="Population Density")
-pop_den_data_less = pop_den_data_less.sort(['Country Code', 'Year'], ascending=True)
+pop_den_data_less = pop_den_data_less.sort_values(by = ['Country Code', 'Year'], ascending=True)
 pop_den_data_less = pop_den_data_less.rename(columns={"Country Name": "Countries", 'Country Code': 'iso3', 'Year': 'year'})
 pop_den_data_less['year'] = pop_den_data_less['year'].astype(int)
 
@@ -99,29 +139,7 @@ for i in (result["g_whoregion"]):
 result["color"] = pd.DataFrame(region_list)
 
 # Animate the Plot Year over Year
-"""plt.figure(2)
-blue_patch = mpatches.Patch(color='b', label='Europe')
-green_patch = mpatches.Patch(color='g', label='Africa')
-red_patch = mpatches.Patch(color='r', label='Americas')
-cyan_patch = mpatches.Patch(color='c', label='Western Pacific')
-mag_patch = mpatches.Patch(color='m', label='Eastern Mediterranean')
-yel_patch = mpatches.Patch(color='y', label='Southeast Asia')
-
-def animate_plot(col, axes):
-    while True:
-        for year in years:
-            plt.legend(handles=[blue_patch, green_patch, red_patch, cyan_patch, mag_patch, yel_patch])
-            result_by_year = result[result['year'] == year]
-            plt.title(year)
-            #plt.plot(log(result_by_year[col]),log(result_by_year['e_inc_100k']), "x", color=colors[(year-2000)%len(colors)])
-            plt.scatter(x = log(result_by_year[col]), y = log(result_by_year['e_inc_100k']), c = result["color"].values.tolist(), alpha = 0.8)
-            plt.axis(axes)
-            plt.xlabel("log pop dens")
-            plt.ylabel("log incidence/100k")
-            plt.draw()
-            plt.pause(pause_time)
-            plt.clf() #I have yet to figure out how to exit this loop
-
+"""
 #plt.figure(2)
 #animate_plot("Population Density", axis_pop_dens)
 
@@ -133,39 +151,6 @@ animate_plot('Health Expenditure per capita', axis_health_exp)
 """
 
 # All 3?
-def animate_one(col, size):
-    plt.legend(handles=[blue_patch, green_patch, red_patch, cyan_patch, mag_patch, yel_patch])
-    result_by_year = result[result['year'] == year]
-    plt.title(str(year) + " " + col)
-    plt.scatter(x = log(result_by_year[col]), y = log(result_by_year['e_inc_100k']), c = result["color"].values.tolist(), alpha = 0.8)
-    plt.axis(size)
-    plt.xlabel("log " + col)
-    plt.ylabel("log incidence/100k")
-    plt.draw()
-    
-blue_patch = mpatches.Patch(color='b', label='Europe')
-green_patch = mpatches.Patch(color='g', label='Africa')
-red_patch = mpatches.Patch(color='r', label='Americas')
-cyan_patch = mpatches.Patch(color='c', label='Western Pacific')
-mag_patch = mpatches.Patch(color='m', label='Eastern Mediterranean')
-yel_patch = mpatches.Patch(color='y', label='Southeast Asia')
-
-"""while True:
-    for year in years:
-        plt.figure(2)
-        animate_one("Population Density", axis_pop_dens)
-        plt.figure(3)
-        animate_one("Health Expenditure per capita", axis_health_exp)
-        plt.figure(4)
-        animate_one("GDP per capita", axis_gdp)
-        plt.pause(pause_time)
-        plt.figure(2)
-        plt.clf() 
-        plt.figure(3)
-        plt.clf() 
-        plt.figure(4)
-        plt.clf() """
-
 try:
     while True:
         for year in years:
@@ -183,10 +168,7 @@ try:
             plt.figure(4)
             plt.clf() 
 except KeyboardInterrupt:
-    pass
-        
-       #plt.clf() 
-
+    plt.clf() 
 
 
 
